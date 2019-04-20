353b709d520eb237fd971170ed0b4a20457d7d6c
Rohith <17rohith29@gmail.com>
2019-04-20 13:06:26 -0400

    Implemented the streeming basics

--- main.py
+++ main.py
@@ -29,10 +29,47 @@ def solveNaieve(lst): # lst is sorted
 
 def solveBasic(lst): # list is sorted based of begin time
     time = 0
+    number_processed = 0
+    previous_processed = None
     last_begin_time = lst[len(lst) - 1][0]
+    to_process = [] # lst that will store all the elements that we need to proecess
 
     while time < last_begin_time:
-        to_process = [] # lst that will store all the elements that we need to proecess
+        if previous_processed is None:
+            previous_processed = time
+            while lst[0][0] == time:
+                to_process.append(lst.pop(0))
+        else:
+            while lst[0][0] <= time and lst[0][0] >= previous_processed:
+                to_process.append(lst.pop(0))
+            previous_processed = time
+            
+        processedItem = solveAlgo1(to_process)
+        number_processed += 1
+        # update time
+        time += processedItem[2]
+        # removing all in processedItem that has time > deadline
+        temp = []
+        for i in to_process:
+            if time < i[1]:
+                temp.append(i)
+        to_process = temp
+
+    # now we must finish processeing the to_process
+    while len(to_process) != 0:
+        processedItem = solveAlgo1(to_process)
+        number_processed += 1
+        # update time
+        time += processedItem[2]
+        # removing all in processedItem that has time > deadline
+        temp = []
+        for i in to_process:
+            if time < i[1]:
+                temp.append(i)
+        to_process = temp
+
+    return number_processed
+
 
 def main(): # this deals with all the preprocessing required to run code.
     n = int(input()) # number of processes
