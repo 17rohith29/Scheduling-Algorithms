64fa4e69ff189fe7c5e6bdad566dd94df82778c0
Rohith <17rohith29@gmail.com>
2019-05-02 20:07:12 -0400

    a

--- main.py
+++ main.py
@@ -27,6 +27,61 @@ def solveNaieve(lst): # lst is sorted
 
     return ans
 
+# greedy algorithm that takes the element with the shortest time
+def solveAlgo1(to_process):
+    # always take the element with shortest time
+    return to_process[0]
+    
+# greedy algorithm that takes the element with the first deadline 
+def solveAlgo2(to_process):
+    temp = None
+    for i in to_process:
+        if temp is None or temp[1] > i[1]:
+            temp = i
+    return temp
+
+# best algorithm
+def solveAlgo3(to_process, time): # sorting O(nlogn) + O(n^2) = O(n^2)
+    # sort by deadline
+    to_process = sorted(to_process, key = lambda x: x[1])
+    res = 0 #index that we need to return 
+    mn = 0
+    for i in range(0, len(to_process)):
+        cnt = 0
+        t = time
+        for j in range(i, len(to_process)):
+            cur = to_process[j]
+            if time + cur[2] <= cur[1]:
+                cnt += 1
+                time += cur[2]
+            else:
+                if cnt > mn:
+                    mn = cnt
+                    res = i
+    return to_process[res]
+
+def solveAlgo4(to_process, time): # O(nlogn))
+    # sort by deadline
+    to_process = sorted(to_process, key = lambda x: x[1])
+    res = 0 #index that we need to return 
+    cnt = 0
+    start, end = 0, 0
+    while start < len(to_process):
+        count = 0
+        while end < len(to_process):
+            cur = to_process[end]
+            if time + cur[2] <= cur[1]:
+                time += cur[2]
+                count += 1
+            else:
+                break
+        if count < cnt:
+            cnt = count
+            res = start
+        time -= to_process[start][2] # removing the processing time of start
+        start += 1
+    return to_process[res]
+
 def solveBasic(lst): # list is sorted based of begin time
     time = 0
     number_processed = 0
@@ -40,18 +95,18 @@ def solveBasic(lst): # list is sorted based of begin time
             while lst[0][0] == time:
                 to_process.append(lst.pop(0))
         else:
-            while lst[0][0] <= time and lst[0][0] >= previous_processed:
+            while lst[0][0] <= time and lst[0][0] >= previous_processed and lst[0][1] > time:
                 to_process.append(lst.pop(0))
             previous_processed = time
             
-        processedItem = solveAlgo1(to_process)
+        processedItem = solveAlgo1(to_process) # returns the processed item
         number_processed += 1
         # update time
         time += processedItem[2]
         # removing all in processedItem that has time > deadline
         temp = []
         for i in to_process:
-            if time < i[1]:
+            if time < i[1]: # remove if already past deadline (or add if not here)
                 temp.append(i)
         to_process = temp
 
@@ -88,4 +143,5 @@ def main(): # this deals with all the preprocessing required to run code.
     ans = solveNaieve(copy.deepcopy(allProcesses))
     # ans = solveBasic(copy.deepcopy(allProcesses))
     print("The Best set of jobs has number {}".format(ans))
+
 main()
